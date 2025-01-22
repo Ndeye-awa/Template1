f09f62bffdda242d2938c4c9570f6198f2ef04df
Ndèye Awa Goumbala Sambe <goumbalasambe@gmail.com>
2025-01-22 13:05:24 +0000

    deploiement

---
+++ treactHtml/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./*.html", // Indique où chercher les classes Tailwind dans ton HTML
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "rgb(100 21 255)", // Ta couleur personnalisée
+        customPurple: "#290573",
+        customBlue: "#5011cc",
+        customGrey: "#243e63",
+        customGray: "#7c8ba1",
+      },
+    },
+  },
+  plugins: [],
+};
